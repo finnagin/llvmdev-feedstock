4d9de3e8c856ac988e828c046a79447717fa72e1
Finn Womack <flan313@gmail.com>
2024-10-14 12:37:13 -0700

    add stage0 compiler build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,49 @@
 echo on
 
+mkdir stage0
+cd stage0
+
+REM Stage0 binaries directory; used in stage1.
+set "stage0_bin_dir=%cd%\bin"
+set stage0_cmake_flags=^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DLLVM_ENABLE_ASSERTIONS=OFF ^
+  -DLLVM_INSTALL_TOOLCHAIN_ONLY=ON ^
+  -DLLVM_TARGETS_TO_BUILD="AArch64;ARM;X86" ^
+  -DLLVM_BUILD_LLVM_C_DYLIB=ON ^
+  -DCMAKE_INSTALL_UCRT_LIBRARIES=ON ^
+  -DPython3_FIND_REGISTRY=NEVER ^
+  -DLLDB_RELOCATABLE_PYTHON=1 ^
+  -DLLDB_EMBED_PYTHON_HOME=OFF ^
+  -DCMAKE_CL_SHOWINCLUDES_PREFIX="Note: including file: " ^
+  -DLLVM_ENABLE_LIBXML2=FORCE_ON ^
+  -DLLDB_ENABLE_LIBXML2=OFF ^
+  -DCLANG_ENABLE_LIBXML2=OFF ^
+  -DCMAKE_C_FLAGS="-DLIBXML_STATIC" ^
+  -DCMAKE_CXX_FLAGS="-DLIBXML_STATIC" ^
+  -DLLVM_ENABLE_RPMALLOC=ON ^
+  -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;lld;compiler-rt;lldb;openmp" ^
+  -DCLANG_DEFAULT_LINKER=lld ^
+  -DLIBXML2_INCLUDE_DIR=%libxmldir%/include/libxml2 ^
+  -DLIBXML2_LIBRARIES=%libxmldir%/lib/libxml2s.lib ^
+  -DPython3_ROOT_DIR=%PYTHONHOME% ^
+  -DCOMPILER_RT_BUILD_PROFILE=OFF ^
+  -DCOMPILER_RT_BUILD_SANITIZERS=OFF
+
+REM We need to build stage0 compiler-rt with clang-cl (msvc lacks some builtins).
+cmake -GNinja %stage0_cmake_flags% ^
+  -DCMAKE_C_COMPILER=clang-cl.exe ^
+  -DCMAKE_CXX_COMPILER=clang-cl.exe ^
+  %SRC_DIR%/llvm
+cd..
+
 mkdir build
 cd build
 
 REM remove GL flag for now
 set "CXXFLAGS=-MD"
-set "CC=cl.exe"
-set "CXX=cl.exe"
+set "CC=%stage0_bin_dir%/clang-cl.exe"
+set "CXX=%stage0_bin_dir%/clang-cl.exe"
 
 cmake -G "Ninja" ^
     -DCMAKE_BUILD_TYPE="Release" ^
